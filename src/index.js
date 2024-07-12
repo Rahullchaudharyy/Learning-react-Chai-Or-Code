af5f372a15be4cf5e192461788125095c778ba2f
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-12 23:43:52 +0530

    Previous-code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App';
+import Chai from './Chai';
+
+const root = ReactDOM.createRoot(document.getElementById('Rahull'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <>
+   
+   <App />
+   <Chai/>
+   
+   </>
 );
