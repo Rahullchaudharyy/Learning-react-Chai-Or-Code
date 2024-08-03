bffabaf3e1d56922a67234d3f3e960b631aff596
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-08-03 16:51:47 +0530

    Making with the appwrite

---
+++ app-write/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITTER_URL)
+  return (
+    <>
+     <h1 className='bg-orange-300'>helo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ app-write/src/conf/config.js
@@ -0,0 +1,12 @@
+const conf = {
+ appWriteUrl :String(import.meta.env.VITE_APPWRITTER_URL),
+ appWriteProjectId:String(import.meta.env.VITE_APPWRITTER_PROJECT_ID),
+ appWriteDatabaseId:String(import.meta.env.VITE_APPWRITTER_DATABASE_ID),
+ appWriteCollectionId:String(import.meta.env.VITE_APPWRITTER_COLLECTION_ID),
+ appWriteBucketId:String(import.meta.env.VITE_APPWRITTER_BUCKET_ID)
+
+
+}
+
+
+export default conf
